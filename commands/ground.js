64f2fc5ec4a0e51309464d6aa57628327881475b
Thunderzapper <justinabossmlg@gmail.com>
2022-07-22 11:24:07 -0400

    finish slash commands

--- commands/ground.js
+++ commands/ground.js
@@ -1,10 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ground')
-		.setDescription('want to FUCK someone over? ground them and watch them cry.'),
+		.setDescription('FUCKJ your users! ground them because they SUCK')
+		.addUserOption(option => 
+			option.setName('user')
+				.setDescription('Select a user')
+				.setRequired(true))
+		.addStringOption(option => 
+			option.setName('reason')
+				.setDescription('Enter a reason')
+				.setRequired(false)),
 	async execute(interaction) {
-		await interaction.reply('SOON!');
-	},
+		const user = interaction.options.getUser('user');
+		const string = interaction.options.getString('reason');
+
+		const GoEmbed = new MessageEmbed()
+		.setTitle(`Sorry bro. You're grounded for ${string} `)
+		.setDescription(`${user} is grounded grounded grounded grounded grounded for infinity years. Now ${user} Go to your room right now!`)
+		.setImage('https://cdn.discordapp.com/attachments/984665993580265496/995554130137452574/unknown.png')
+		.setColor(0xF44336)
+		await interaction.reply({ embeds: [GoEmbed] });
+	}
 };
--- commands/unground.js
+++ commands/unground.js
@@ -1,15 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('unground')
 		.setDescription('Reward your users!')
-		.addUserOption(option => option.setName('target').setDescription('Select a user'))
-		.addStringOption(option => option.setName('reason').setDescription('Enter a reason')),
-
+		.addUserOption(option => 
+			option.setName('user')
+				.setDescription('Select a user')
+				.setRequired(true))
+		.addStringOption(option => 
+			option.setName('reason')
+				.setDescription('Enter a reason')
+				.setRequired(false)),
 	async execute(interaction) {
+		const user = interaction.options.getUser('user');
+		const string = interaction.options.getString('reason');
+
 		const GoEmbed = new MessageEmbed()
-		.setTitle(`You're now Ungrounded! Enjoy!`)
+		.setTitle(`You're now Ungrounded for ${string}! Enjoy!`)
+		.setDescription(`${user} is ungrounded 😃😃😃`)
 		.setImage('https://cdn.discordapp.com/attachments/984665993580265496/995554289718136862/unknown.png')
 		.setColor(0x0ACE00)
 		await interaction.reply({ embeds: [GoEmbed] });
