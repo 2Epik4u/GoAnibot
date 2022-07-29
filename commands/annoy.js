d71551ddb9effc9aa01b199f0a2eef4752082731
sparrkz <71980947+shezsparrkz@users.noreply.github.com>
2022-07-29 14:55:52 -0400

    what

--- commands/annoy.js
+++ commands/annoy.js
@@ -10,11 +10,7 @@ module.exports = {
 		.setDescription('Plays a sound on the computer of the person hosting the bot')
 		.addAttachmentOption(option =>
 			option.setName('sound')
-<<<<<<< HEAD
-				.setDescription('Sound for the bot to play (MUST BE MP3)')),
-=======
 				.setDescription('sound for the bot to play')),
->>>>>>> 521ba98 (3.2 + i don't need nitpick shit epik)
 	async execute(interaction) {
 		interaction.reply({ content: 'ok' });
 		const file = interaction.options.getAttachment('sound');
